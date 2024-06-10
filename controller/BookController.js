10ce179300eb0f4dc5e611aae3e7bc696903abb8
mooner92 <chlaudgjs0989@gmail.com>
2024-06-10 16:28:49 +0900

    add partial select

--- controller/BookController.js
+++ controller/BookController.js
@@ -3,10 +3,36 @@ const {StatusCodes} = require('http-status-codes');
 
 
 const allBooks = (req,res)=>{
-    res.json('전체도서조회');
+    let sql = "SELECT * FROM books";
+
+    conn.query(sql,
+        (err,results) => {
+            if(err){
+                console.log(err);
+                return res.status(StatusCodes.BAD_REQUEST).end(); //BAD REQ
+            }
+    
+            res.status(StatusCodes.OK).json(results);
+        })
+    //res.json('전체도서조회');
 }
 const bookDetail = (req,res)=>{
-    res.json('전체도서조회');
+    let {id} = req.params;
+
+    let sql = "SELECT * FROM books WHERE id=?";
+
+    conn.query(sql,id,
+        (err,results) => {
+            if(err){
+                console.log(err);
+                return res.status(StatusCodes.BAD_REQUEST).end(); //BAD REQ
+            }
+            if(results[0])
+                return res.status(StatusCodes.OK).json(results);
+            else
+                res.status(StatusCodes.NOT_FOUND).end();
+        })
+    //res.json('전체도서조회');
 }
 const booksByCategory = (req,res)=>{
     res.json('전체도서조회');
