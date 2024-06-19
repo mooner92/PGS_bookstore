75907e82ea8f677adb9cd62bb4f99f7153662541
mooner92 <chlaudgjs0989@gmail.com>
2024-06-19 11:33:38 +0900

    add

--- controller/BookController.js
+++ controller/BookController.js
@@ -4,7 +4,21 @@ const {StatusCodes} = require('http-status-codes');
 
 const allBooks = (req,res)=>{
     let {category_id,news} = req.query;
-    if(category_id){
+    if(category_id && news){
+        let sql = "SELECT * FROM books WHERE category_id=?";
+
+        conn.query(sql,category_id,
+            (err,results) => {
+                if(err){
+                    console.log(err);
+                    return res.status(StatusCodes.BAD_REQUEST).end(); //BAD REQ
+                }
+                if(results.length)
+                    return res.status(StatusCodes.OK).json(results);
+                else
+                    res.status(StatusCodes.NOT_FOUND).end();
+            })
+    }else if(category_id){
         let sql = "SELECT * FROM books WHERE category_id=?";
 
         conn.query(sql,category_id,
@@ -36,7 +50,8 @@ const allBooks = (req,res)=>{
 const bookDetail = (req,res)=>{
     let {id} = req.params;
 
-    let sql = "SELECT * FROM books WHERE id=?";
+    let sql = `SELECT * FROM books LEFT JOIN category 
+    ON books.category_id = category.id WHERE books.id=?`;
 
     conn.query(sql,id,
         (err,results) => {
